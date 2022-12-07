2f1d2ade2b1725f8a911188bc9c560e9a3956267
10Sadique <jafarsjahan@gmail.com>
2022-12-07 15:53:20 +0600

    project initalized

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        './pages/**/*.{js,ts,jsx,tsx}',
+        './components/**/*.{js,ts,jsx,tsx}',
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                space: "'Space Grotesk', sans-serif",
+            },
+            colors: {
+                'green-bright': '#15d155',
+                'dark-gray': '#646464',
+            },
+        },
+    },
+    plugins: [],
+};
