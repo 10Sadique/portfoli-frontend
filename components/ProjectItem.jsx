6994196eee8cc77ea9c3159d257e2cbaab8b780b
10Sadique <jafarsjahan@gmail.com>
2022-12-08 22:13:00 +0600

    projects section complete

---
+++ components/ProjectItem.jsx
@@ -0,0 +1,27 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react';
+
+const ProjectItem = ({ title, image, porjectUrl, tech }) => {
+    return (
+        <div className="relative flex items-center justify-center w-full h-auto p-3 bg-green-bright/20 rounded-xl group hover:bg-gradient-to-br from-green-bright to-green-500">
+            <Image
+                className="w-full rounded-xl group-hover:opacity-10 "
+                src={image}
+                width={1000}
+                alt=""
+            />
+            <div className="hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
+                <h3 className="text-3xl tracking-wider text-center">{title}</h3>
+                <p className="pt-2 pb-4 text-center">{tech}</p>
+                <Link href={`${porjectUrl}`}>
+                    <p className="px-5 py-3 text-lg font-bold text-center bg-black rounded-lg cursor-pointer">
+                        More Info
+                    </p>
+                </Link>
+            </div>
+        </div>
+    );
+};
+
+export default ProjectItem;
---
+++ components/Projects.jsx
@@ -0,0 +1,77 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react';
+import ProjectItem from './ProjectItem';
+import lapStore from '../public/assets/projects/lapStore.png';
+import devQuiz from '../public/assets/projects/devQuiz.png';
+import hotelTs from '../public/assets/projects/hotelTs.png';
+import portfolio from '../public/assets/projects/portfolio.png';
+import soloCarpentry from '../public/assets/projects/soloCarpentry.png';
+import t3verse from '../public/assets/projects/t3verse.png';
+import tsTravels from '../public/assets/projects/tsTravels.png';
+import tureVolunteer from '../public/assets/projects/tureVolunteer.png';
+
+const Projects = () => {
+    return (
+        <div className="w-full px-5 py-16">
+            <div className="max-w-[1240px] mx-auto">
+                <p className="py-4 text-xl tracking-widest uppercase text-green-bright">
+                    Projects
+                </p>
+                <h2 className="pt-4 pb-5">{`What I've Built`}</h2>
+                <div className="grid md:grid-cols-2 gap-7">
+                    <ProjectItem
+                        title="LapStore"
+                        image={lapStore}
+                        projectUrl="/lapStore"
+                        tech="React JS"
+                    />
+                    <ProjectItem
+                        title="Solo Carpentry"
+                        image={soloCarpentry}
+                        projectUrl="/soloCarpentry"
+                        tech="React JS"
+                    />
+                    <ProjectItem
+                        title="Portfolio"
+                        image={portfolio}
+                        tech="Next JS"
+                        projectUrl="/portfolio"
+                    />
+                    <ProjectItem
+                        title="True Volunteer"
+                        image={tureVolunteer}
+                        projectUrl="/tureVolunteer"
+                        tech="React JS"
+                    />
+                    <ProjectItem
+                        title="T3 Verse"
+                        image={t3verse}
+                        projectUrl="/t3verse"
+                        tech="React JS"
+                    />
+                    <ProjectItem
+                        title="TS Travels"
+                        image={tsTravels}
+                        projectUrl="/tsTravels"
+                        tech="React JS"
+                    />
+                    <ProjectItem
+                        title="DevQuiz"
+                        image={devQuiz}
+                        projectUrl="/devQuiz"
+                        tech="React JS"
+                    />
+                    <ProjectItem
+                        title="Hotel TS"
+                        image={hotelTs}
+                        projectUrl="/hotelTs"
+                        tech="React JS"
+                    />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Projects;
